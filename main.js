0d0e1343d2955c2b036334fbfe418bf9286e32d9
Djazy-Faradj <djazy.faradj@outlook.com>
2024-10-18 01:37:13 -0400

    Learnt about variables and functions

---
+++ main.js
@@ -0,0 +1,7 @@
+function displayGreeting(name) {
+    const message = `Welcome to this program, ${name}!`
+    return message;
+    console.log(message);   
+}
+
+console.log(displayGreeting('Djazy'));
